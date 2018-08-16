2c115d3fb115c34af14f294d3d98ecb2b870a66b
LAPTOP-MNMV8UG3\Vulcan <1079021229@qq.com>
2018-08-17 03:30:39 +0800

    OK
    算法还有问题

--- sketch_aug16b/sketch_aug16b.ino
+++ sketch_aug16b/sketch_aug16b.ino
@@ -1,8 +1,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
+#include "Suanfa.h"
 #define   OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);                   //初始化显示屏
+Suanfa sf;
 
 const int INT0A = 18;
 const int INT0B = 19;
@@ -11,11 +12,24 @@ const int INT1B = 5;
 const int INT2A = 3;
 const int INT2B = 4;
 
+const double d = 160; //两轮间距160mm
+const double wheel_d = 38;//轮直径38mm
+
 int x = 0;
 int y = 0;
 int p = 0;
+
+double x1 = 0;
+double y1 = 0;
+double p1 = 0;
+
+double x2 = 0;
+double y2 = 0;
+double p2 = 0;
+
 int time = 0;
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
   display.begin(SSD1306_SWITCHCAPVCC);                    //显示屏初始化
   display.display();
@@ -31,6 +45,29 @@ void setup() {
 }
 
 void loop() {
+  x1 = double(x)/20*PI*wheel_d;//行走的距离 单位mm；
+  y1 = double(y)/20*PI*wheel_d;//行走的距离 单位mm；
+  p1 = double(p)/20*PI*wheel_d;//行走的距离 单位mm；
+
+  p2 = (y1 - p1)/d;
+  p2 = p2/PI*180;
+
+  x2 = x1 + (y1 - p1)/2;
+  y2 = (y1+p1)/2;
+
+  sf.realXY(x2,y2,p2);
+  
+  Serial.print("A:");  //串口显口
+  Serial.print(0);                 //串口显示
+  Serial.print("B:");  //串口显口
+  Serial.print(0);                 //串口显示
+  Serial.print("X:");  //串口显口
+  Serial.print(sf.x);                 //串口显示
+  Serial.print("Y:");                   //串口显示
+  Serial.print(sf.y);                 //串口显示
+  Serial.print("P:");                   //串口显示
+  Serial.println(p2);  //串口显口
+  
   view(x, y, p);
 }
 //中断0调用函数
